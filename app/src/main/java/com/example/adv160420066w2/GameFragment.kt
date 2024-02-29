4f7fc68cca170e5cabd55fa2745c285825950215
Renky32 <s160420066@student.ubaya.ac.id>
2024-02-29 15:33:13 +0700

    init

---
+++ app/src/main/java/com/example/adv160420066w2/GameFragment.kt
@@ -0,0 +1,48 @@
+package com.example.adv160420066w2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.adv160420066w2.databinding.FragmentGameBinding
+import java.util.NavigableMap
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [GameFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class GameFragment : Fragment() {
+    private lateinit var binding: FragmentGameBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(arguments != null) {
+            val playerName =
+                GameFragmentArgs.fromBundle(requireArguments()).playerName
+            binding.txtTurn.text = "$playerName's Turn"
+        }
+        binding.btnBack.setOnClickListener{
+            val action = GameFragmentDirections.actionMainFragment()
+            Navigation.findNavController(it).navigate(action    )
+        }
+    }
+
+
+}
